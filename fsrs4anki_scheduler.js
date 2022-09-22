d12ca3f8d0093eff57324f624e49e2a389fd443f
Jarrett Ye <jarrett.ye@outlook.com>
2022-09-22 21:56:17 +0800

    fix intervals of different rating are equal (#13)

--- fsrs4anki_scheduler.js
+++ fsrs4anki_scheduler.js
@@ -1,4 +1,4 @@
-// FSRS4Anki v1.3.0 Scheduler
+// FSRS4Anki v1.3.1 Scheduler
 // The latest version will be released on https://github.com/open-spaced-repetition/fsrs4anki
 
 // Default parameters of FSRS4Anki
@@ -77,14 +77,18 @@ if (states.current.normal?.new !== undefined | Object.hasOwn(states.current.filt
     customData.easy.d = constrain_difficulty(last_d + retrievability - 2 + 0.1);
     customData.easy.s = next_stability(customData.easy.d, last_s, retrievability);
 
+    const hard_interval = constrain_interval(last_s * hardInterval);
+    const good_interval = Math.max(constrain_interval(customData.good.s), hard_interval + 1);
+    const easy_interval = Math.max(constrain_interval(customData.easy.s * easyBonus), good_interval + 1);
+
     if (states.hard.normal?.review) {
-        states.hard.normal.review.scheduledDays = constrain_interval(last_s * hardInterval);
+        states.hard.normal.review.scheduledDays = hard_interval;
     }
     if (states.good.normal?.review) {
-        states.good.normal.review.scheduledDays = constrain_interval(customData.good.s);
+        states.good.normal.review.scheduledDays = good_interval;
     }
     if (states.easy.normal?.review) {
-        states.easy.normal.review.scheduledDays = constrain_interval(customData.easy.s * easyBonus);
+        states.easy.normal.review.scheduledDays = easy_interval;
     }
 }
 
