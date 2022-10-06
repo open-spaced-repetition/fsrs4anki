805bba9f5ec0c044463a79b73c411096d96cb767
Jarrett Ye <jarrett.ye@outlook.com>
2022-10-07 01:28:12 +0800

    Fix/constrain parameters (#60)
    * constrain initial stability and difficulty
    
    * constrain range of parameters
    
    * update default parameters

--- fsrs4anki_scheduler.js
+++ fsrs4anki_scheduler.js
@@ -1,9 +1,9 @@
-// FSRS4Anki v3.0.0 Scheduler
+// FSRS4Anki v3.0.1 Scheduler
 set_version();
 // The latest version will be released on https://github.com/open-spaced-repetition/fsrs4anki
 
 // Default parameters of FSRS4Anki for global
-var w = [1, 1, 5, -1, -1, 0.1, 1.5, -0.2, 0.8, 2, -0.2, 0.2, 1];
+var w = [1, 1, 5, -0.5, -0.5, 0.2, 1.4, -0.12, 0.8, 2, -0.2, 0.2, 1];
 // The above parameters can be optimized via FSRS4Anki optimizer.
 
 // User's custom parameters for global
@@ -147,11 +147,11 @@ function init_states() {
 }
 
 function init_difficulty(rating) {
-    return +(w[2] + w[3] * (ratings[rating] - 3)).toFixed(2);
+    return +constrain_difficulty(w[2] + w[3] * (ratings[rating] - 3)).toFixed(2);
 }
 
 function init_stability(rating) {
-    return +(w[0] + w[1] * (ratings[rating] - 1)).toFixed(2);
+    return +Math.max(w[0] + w[1] * (ratings[rating] - 1), 0.1).toFixed(2);
 }
 
 function convert_states() {
@@ -226,7 +226,7 @@ function is_empty() {
 }
 
 function set_version() {
-    const version = "3.0.0";
+    const version = "3.0.1";
     customData.again.v = version;
     customData.hard.v = version;
     customData.good.v = version;
