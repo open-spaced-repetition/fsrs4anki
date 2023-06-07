dae7628a7922afc0ccb3c6d476348deeb82da934
Luc Mcgrady <lucmcgrady@gmail.com>
2023-06-07 13:37:54 +0100

    Fix: Duplicated version number also file targets not correctly named

--- package/setup.py
+++ package/setup.py
@@ -1,11 +1,14 @@
 import setuptools
+import sys
+
+version = sys.argv.pop(2)
 
 with open("requirements.txt", "r") as f:
     requires = [a.strip('\n') for a in f.readlines()]
 
 setuptools.setup(
     name="fsrs4anki_optimizer",
-    version="3.21.2",
+    version=version,
     packages=setuptools.find_packages(),
     install_requires=requires
 )
