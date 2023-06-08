ccf32e2860009bb01e2048a89a11b7c9d2687cdf
Luc Mcgrady <lucmcgrady@gmail.com>
2023-06-08 02:52:20 +0100

    Fix/Remove duplicated version number. (#285)

--- package/setup.py
+++ package/setup.py
@@ -1,11 +1,14 @@
 import setuptools
 
+with open("version.txt", "r") as f:
+    version = f.read()
+
 with open("requirements.txt", "r") as f:
     requires = [a.strip('\n') for a in f.readlines()]
 
 setuptools.setup(
     name="fsrs4anki_optimizer",
-    version="3.21.2",
+    version=version,
     packages=setuptools.find_packages(),
     install_requires=requires
 )
